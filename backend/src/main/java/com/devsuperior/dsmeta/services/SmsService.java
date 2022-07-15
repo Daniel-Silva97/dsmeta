8f51982ecc471805c57d9358e4a492af37e82c48
Daniel da silva pedro <danielsp699@outlook.com>
2022-07-15 09:36:47 -0300

    Heroku SMS Fail - String DecimalFormat

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
+
 @Service
 public class SmsService {
 
@@ -35,7 +37,7 @@ public class SmsService {
         String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 
         String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-                + " com um total de R$ " + String.format("%.2f", sale.getAmount());
+                + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
 
         Twilio.init(twilioSid, twilioKey);
 
